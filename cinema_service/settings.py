f43b8a059c376e0e00625c0afbb5e9070b1eacc1
Ivan Khomych <ivan.khomychh@gmail.com>
2025-12-15 15:16:49 +0200

    fix authentication views users

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -138,10 +138,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
     "DEFAULT_PERMISSION_CLASSES": [
-      "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly",
+        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
